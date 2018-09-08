bb5319e8930602750366f85abc4f73f568ef3142
Alexander Nathanail <adnathanail@hotmail.co.uk>
2018-09-08 17:35:38 +0100

    Fix app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ItemListComponent } from './item-list/item-list.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/item/list', pathMatch: 'full' },
   { path: 'item', children: [
     { path: '', redirectTo: 'list', pathMatch: 'full' },
     { path: 'list', component: ItemListComponent },
-    { path: ':id', component: ItemDetailComponent }
   ]},
 ];
 
